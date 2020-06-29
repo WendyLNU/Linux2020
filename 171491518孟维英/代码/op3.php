ffed73a975cc22dc3aed28c3fa1a994b3d67c38b
1552436 <56257033+1552436@users.noreply.github.com>
2020-06-30 00:42:54 +0800

    Create op3.php

---
+++ 171491518孟维英/代码/op3.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>任选课成绩</title>
+<link href="css.css" rel="stylesheet" type="text/css">
+</head>
+
+<body background="image/下载.jpg">
+<table width="600" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr>
+    <td width="294" align="center">课程名字</td>
+    <td width="89" align="center">学分</td>
+    <td width="105" align="center">分数</td>
+    <td width="158" align="center">考试时间</td>
+    <td width="132" align="center">类型</td>
+  </tr>
+    <tr>
+    <td width="100" align="center">&nbsp;</td>
+    <td width="600" align="center">&nbsp;</td>
+  </tr>
+<?php 
+require("conn.php");
+$sql = "Select * From my_university_mark where course_type='任选'";
+$rs=new com("adodb.recordset");
+$rs->open($sql,$db,1,1);//执行语句,返回记录集 
+while(! $rs->eof)
+{
+?>
+  <tr>
+    <td align="center"><?php echo $rs->Fields("course_name")->value; ?></td>
+    <td align="center"><?php echo $rs->Fields("course_mark")->value; ?></td>
+    <td align="center"><?php echo $rs->Fields("course_score")->value; ?></td>
+    <td align="center"><?php echo $rs->Fields("course_time")->value; ?></td>
+    <td align="center"><?php echo $rs->Fields("course_type")->value; ?></td>
+  </tr>
+<?php
+ echo  $rs->MoveNext(); 
+} ?>
+</table>
+
+</body>
+</html>
